87f3918a74df885c112491d67c4e0d8a05dd086d
pinchas vaknin <pinchasvak@gmail.com>
2025-10-23 16:03:34 +0300

    Initial code + Welcome functions

---
+++ Stage0/Program4652.cs
@@ -0,0 +1,17 @@
+﻿partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcome4652();
+        Welcome2851();
+        Console.ReadKey();
+    }
+
+    static partial void Welcome2851();
+    private static void Welcome4652()
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.Write("{0}, welcome to my first console application", name);
+    }
+}
