ab9c296f3525a2250d05cc41db5605a5b6c1a6c7
ami <xnh123@gmail.com>
2025-10-25 22:34:54 +0300

    add im here

---
+++ Stage0/Program2851.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Stage0
+{
+    partial class Program
+    {
+        private static void Welcome2851()
+            {
+                {
+                    Console.WriteLine("I'm here");
+                }
+            }
+    }
+}
+
