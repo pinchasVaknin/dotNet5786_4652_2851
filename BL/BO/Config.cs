3ba2ec67223e77066d5f81cb51966d2072ed2cae
pinchas vaknin <pinchasvak@gmail.com>
2025-12-01 13:32:54 +0200

    *

---
+++ BL/BO/Config.cs
@@ -0,0 +1,22 @@
+﻿namespace BO;
+
+public class Config
+{
+    public DateTime Clock { get; set; }
+    public int AdminId { get; set; }
+    public string AdminPassword { get; set; }
+
+    public string? CompanyAdress { get; set; }
+    public double? Latitude { get; set; }
+    public double? Longitude { get; set; }
+
+    public double? MaxAirDistance { get; set; }
+    public double AvgCarSpeed { get; set; }
+    public double AvgMotorcycleSpeed { get; set; }
+    public double AvgBicyleSpeed { get; set; }
+    public double AvgWalkSpeed { get; set; }
+
+    public TimeSpan MaxDelTimeRnge { get; set; }
+    public TimeSpan RiskTimeRnge { get; set; }
+    public TimeSpan UnactiveTimeRnge { get; set; }
+}
