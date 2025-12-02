7f5b9ae24de131b2da84183dd3076f7bdc7eeaf4
ami <xnh123@gmail.com>
2025-12-02 17:22:43 +0200

    ordermanager

--- BL/Helpers/OrderManager.cs
+++ BL/Helpers/OrderManager.cs
@@ -1,8 +1,112 @@
 ﻿namespace Helpers;
+
+
 using DalApi;
+using DO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 internal static class OrderManager
 {
     private static IDal s_dal = Factory.Get;
+
+    internal static void SaveOrder(BO.Order Order)
+    {
+        try
+        {
+            // Map the business object Order to a data object Order
+            DO.Order doOrder = ConvertBoToDoOrder(Order);
+
+            // Create or update the Order in the data access layer
+            if (Order.OrderId == 0)
+                s_dal.Order.Create(doOrder);
+            else
+                s_dal.Order.Update(doOrder);
+        }
+        catch (DalAlreadyExistsException ex)
+        {
+            // Wrap and rethrow any exceptions that occur during the save operation
+            throw new Exception("Failed to save Order", ex);
+        }
+        catch (DalXMLFileLoadCreateException ex)
+        {
+            // Wrap and rethrow any exceptions that occur during the save operation
+            throw new Exception("Failed to save Order", ex);
+        }
+        catch (DalDoesNotExistException ex)
+        {
+            // Wrap and rethrow any exceptions that occur during the save operation
+            throw new Exception("Failed to save Order", ex);
+        }
+    }
+
+    internal static BO.Order GetOrder(int id) // read Order by id
+    {
+        try
+        {
+            // Read the Order from the data access layer
+            DO.Order doOrder = s_dal.Order.Read(id)
+                ?? throw new Exception($"Order with ID={id} does not exist");
+
+            // Build and return the business object representation of the Order
+            return ConvertDoToBoOrder(doOrder);
+        }
+        catch (Exception ex)
+        {
+            // Wrap and rethrow any exceptions that occur during the process
+            throw new Exception("Failed to load Order", ex);
+        }
+    }
+
+    internal static IEnumerable<BO.OrderInList> GetOrders() // read all Orders - query syntax
+    {
+        try
+        {
+            var allOrders = s_dal.Order.ReadAll();
+            var allDeliveries = s_dal.Delivery.ReadAll();
+
+            var query =
+                from o in allOrders
+                join d in allDeliveries 
+                    on o.OrderId equals d.OrderId into ordersdGroup
+
+                let ExpectedDeliveryTime // מיקום שליח מיקום הזמנה 
+
+
+
+
+
+
+
+
+
+                select new BO.OrderInList
+                {
+                    DeliveryId = ordersdGroup.FirstOrDefault()?.DeliveryId,
+                    OrderId = o.OrderId,
+                    TypeOfOrder = (BO.TypeOfOrder)o.TypeOfOrder,
+                    AirDistance = o.AirDistance,
+                    OrderStatus = (BO.OrderStatus)o.OrderStatus,
+                    ScheduleStatus = (BO.ScheduleStatus)o.ScheduleStatus,
+                    TimeLeftToFinish = o.EstimatedTimeToFinish.HasValue ? TimeSpan.FromMinutes(o.EstimatedTimeToFinish.Value) : TimeSpan.Zero,
+                    TotalHandleTime = TimeSpan.FromMinutes(o.TotalHandleTime),
+                    TotalDeliveries = o.TotalDeliveries
+                };
+
+
+
+        }
+        catch (DalXMLFileLoadCreateException ex)
+        {
+            throw new Exception("Failed to load orders list (query syntax)", ex);
+        }
+    }
+
+
+
+
+
+
 }
 
